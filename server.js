ed6509af8ce207c4942b40faaef38da1bfe6199e
Jimmy Arango <jimalaros25@gmail.com>
2021-10-23 17:28:19 -0500

    Archivos Finales

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ import Express from 'express';
 import Cors from 'cors';
 import dotenv from 'dotenv';
 import { conectarBD } from './db/db.js';
-//import jwt from 'express-jwt';
-//import jwks from 'jwks-rsa';
+import jwt from 'express-jwt';
+import jwks from 'jwks-rsa';
 
 import rutasOrdenes from './Routes/ordenes/rutas.js';
 import rutasUsuarios from './Routes/usuarios/rutas.js';
-//import autorizacionEstadoUsuario from './middleware/autorizacionEstadoUsuario.js';
+import autorizacionEstadoUsuario from './middleware/autorizacionEstadoUsuario.js';
 
 dotenv.config({ path: './.env' });
 
@@ -18,22 +18,21 @@ const app = Express();
 app.use(Express.json());
 app.use(Cors());
 
-/**var jwtCheck = jwt({
+var jwtCheck = jwt({
   secret: jwks.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: 'https://misiontic-concesionario.us.auth0.com/.well-known/jwks.json',
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: 'https://devstwo.us.auth0.com/.well-known/jwks.json'
   }),
-  audience: 'api-autenticacion-concesionario-mintic',
-  issuer: 'https://misiontic-concesionario.us.auth0.com/',
-  algorithms: ['RS256'],
+  audience: 'fast-food-delivery',
+  issuer: 'https://devstwo.us.auth0.com/',
+  algorithms: ['RS256']
 });
 
-// 4 y 5: enviarle el token a auth0 para que devuelva si es valido o no
 app.use(jwtCheck);
 
-app.use(autorizacionEstadoUsuario);**/
+app.use(autorizacionEstadoUsuario);
 
 app.use(rutasOrdenes);
 app.use(rutasUsuarios);
